c4c5c2d3dcc77590d39e82d004281aadd9f50de3
gui0910oliveira-hub <gui0910oliveira@gmail.com>
2026-03-19 00:29:18 +0000

    aula 18/3/26

---
+++ src/form.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>form - 18/03/26</h1>
+   <form method="post" action="recebe.php">
+<hr>
+Login<input type="text" id="login" name="login"><br>
+Senha<input type="password" id="senha" name="senha"><br>
+<hr>
+<input type="submit" value="Validar"><br>
+
+</form>
+   
+</body>
+</html>
